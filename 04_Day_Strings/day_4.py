2bf92f524200de3ef45a82eea2402aceee78a6cb
Elliot Park <elliotpark410@gmail.com>
2024-08-11 00:05:02 -0700

    add comment

--- 04_Day_Strings/day_4.py
+++ 04_Day_Strings/day_4.py
@@ -17,6 +17,7 @@ I didn't find anything as rewarding as empowering people.
 That is why I created 30 days of python.'''
 print(multiline_string)
 # Another way of doing the same thing
+# you can update value of string variables 
 multiline_string = """I am a teacher and enjoy teaching.
 I didn't find anything as rewarding as empowering people.
 That is why I created 30 days of python."""
